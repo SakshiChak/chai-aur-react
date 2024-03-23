46e26941506b930b07be372ba6f7a23d0acc2cc5
SakshiChak <mailme.sakshichak@gmail.com>
2024-03-24 00:51:09 +0530

    create basic react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <>
+      <h1>Chai aur React | Sakshi Chak</h1>
+      <h2>Summary</h2>
+      <ol>
+            <li>Create a basic React app: npx create-react-app projectName</li>
+            <li>Navigate to your project directory: cd projecName</li>
+            <li>Start the development server: npm run start</li>
+            <li>To build this project: npm run build</li>
+            
+      </ol>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Chai aur React with Vite | Sakshi Chak</h1>
+    <ol>
+        <li>Create a basic React app With Vite: npm create vite@latest</li>
+        <li>Start the development server: npm run dev</li>
+    </ol>
+    </>
+  )
+}
+
+export default App
