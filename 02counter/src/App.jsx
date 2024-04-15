91a0ef766a0fe5b8a4bbf3b052005767025755ca
SakshiChak <mailme.sakshichak@gmail.com>
2024-04-15 19:13:26 +0530

    increment counter value by 4

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,14 +4,18 @@ import './App.css'
 
 function App() {
   // Use useState hook to create a state variable 'counter' with initial value 10
-  const [counter, setCounter] = useState(10);  // let counter = 10
+  const [counter, setCounter] = useState(8);  // let counter = 8
 
   // Function to increment the counter value
   const addValue = () => {
     // Check if the counter value is less than 20 before incrementing
     if (counter < 20) {
       // Update the counter value using setCounter
-      setCounter(counter + 1);
+      // setCounter(counter + 1);
+      setCounter(prevCounter => prevCounter + 1) //ensures the update is based on the previous state
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
     }
   };
 
