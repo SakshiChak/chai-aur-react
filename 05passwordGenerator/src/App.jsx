5a09d2fd5478625000905caba775cb55ae4366aa
SakshiChak <mailme.sakshichak@gmail.com>
2024-08-12 00:15:23 +0530

    update app.jsx

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -33,11 +33,11 @@ function App() {
 		passwordGenerator();
 	}, [length, numberAllowed, charAllowed, passwordGenerator]);
 	return (
-		<div className="w-full max-w-xl mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-300">
-			<h1 className="text-center text-4xl my-3 font-serif">
-				Password generator
+		<div className="w-full max-w-xl min-h-48 mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-900 text-orange-300">
+			<h1 className="text-center text-4xl my-5 font-serif">
+				Password Generator
 			</h1>
-			<div className="flex shadow rounded-lg overflow-hidden mb-4 text-xl">
+			<div className="flex shadow rounded-lg overflow-hidden mb-6 text-xl">
 				<input
 					type="text"
 					value={password}
@@ -48,47 +48,57 @@ function App() {
 				/>
 				<button
 					onClick={copyPasswordToClipboard}
-					className="outline-none shrink-0 bg-blue-500 hover:bg-blue-700 text-orange-100 font-semibold py-2 px-4 rounded">
+					className="outline-none shrink-0 bg-blue-500 hover:bg-blue-700 text-orange-100 font-semibold py-2 px-4 rounded"
+				>
 					Copy
 				</button>
 			</div>
 			<div className="flex text-xl gap-x-2">
-				<div className="flex items-center gap-x-1">
+				<div className="flex items-center gap-x-2">
 					<input
 						type="range"
 						min={8}
 						max={50}
 						value={length}
-						className="cursor-pointer"
+						className="cursor-pointer accent-blue-500"
 						onChange={(e) => {
 							setLength(e.target.value);
 						}}
 					/>
 					<label className="text-orange-200 ">Length: {length}</label>
 				</div>
-				<div className="flex items-center gap-x-1">
+				<div className="flex items-center gap-x-2 p-2">
 					<input
 						type="checkbox"
 						defaultChecked={numberAllowed}
 						id="numberInput"
+						className="accent-blue-500 h-5 w-5 cursor-pointer hover:accent-blue-700 focus:ring-2 focus:ring-blue-300"
 						onChange={() => {
 							setNumberAllowed((prev) => !prev);
 						}}
 					/>
-					<label htmlFor="numberInput" className="text-orange-200">
+					<label
+						htmlFor="numberInput"
+						className="text-orange-200 cursor-pointer hover:text-orange-400 transition-colors duration-200"
+					>
 						Numbers
 					</label>
 				</div>
-				<div className="flex items-center gap-x-1">
+
+				<div className="flex items-center gap-x-2 p-2">
 					<input
 						type="checkbox"
 						defaultChecked={charAllowed}
 						id="characterInput"
+						className="accent-blue-500 h-5 w-5 cursor-pointer hover:accent-blue-700 focus:ring-2 focus:ring-blue-300"
 						onChange={() => {
 							setCharAllowed((prev) => !prev);
 						}}
 					/>
-					<label htmlFor="characterInput" className="text-orange-200">
+					<label
+						htmlFor="characterInput"
+						className="text-orange-200 cursor-pointer hover:text-orange-400 transition-colors duration-200"
+					>
 						Characters
 					</label>
 				</div>
