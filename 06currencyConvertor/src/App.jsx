d68f95194a1701dc0f712b73f085723d451249e3
SakshiChak <mailme.sakshichak@gmail.com>
2024-08-14 16:31:44 +0530

    update css

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -5,35 +5,35 @@ import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
 
-  const [amount, setAmount] = useState(0)
-  const [from, setFrom] = useState("usd")
-  const [to, setTo] = useState("inr")
-  const [convertedAmount, setConvertedAmount] = useState(0)
-
-  const currencyInfo = useCurrencyInfo(from)
-
-  const options = Object.keys(currencyInfo)
-
-  const swap = () => {
-    setFrom(to)
-    setTo(from)
-    setConvertedAmount(amount)
-    setAmount(convertedAmount)
-  }
+    const [amount, setAmount] = useState(0)
+    const [from, setFrom] = useState("usd")
+    const [to, setTo] = useState("inr")
+    const [convertedAmount, setConvertedAmount] = useState(0)
   
-  const convert = () => {
-    setConvertedAmount(amount * currencyInfo[to])
-  }
+    const currencyInfo = useCurrencyInfo(from)
+  
+    const options = Object.keys(currencyInfo)
+  
+    const swap = () => {
+      setFrom(to)
+      setTo(from)
+      setConvertedAmount(amount)
+      setAmount(convertedAmount)
+    }
+    
+    const convert = () => {
+      setConvertedAmount(amount * currencyInfo[to])
+    }
 
   return (
     <div
-        className="w-full h-screen flex flex-wrap justify-center items-center bg-auto bg-repeat bg-blend-multiply"
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat "
         style={{
-            backgroundImage: `url('https://img.freepik.com/free-vector/digital-money-transfer-technology-background_1017-17454.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1723334400&semt=ais_hybrid')`,
+            backgroundImage: `url('https://static.vecteezy.com/system/resources/previews/019/523/909/non_2x/abstract-currency-digital-finance-technology-modern-currency-exchange-transfer-profit-on-modern-background-futuristic-wave-flowing-blue-vector.jpg')`,
         }}
     >
-        <div className="w-full">
-            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <div className="w-full ">
+            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-zinc-950/30">
                 <form
                     onSubmit={(e) => {
                         e.preventDefault();
@@ -43,10 +43,11 @@ function App() {
                 >
                     <div className="w-full mb-1">
                         <InputBox
+                            className='font-bold text-xl'
                             label="From"
                             amount={amount}
                             currencyOptions={options}
-                            onCurrencyChange={(currency) => setAmount(amount)}
+                            onCurrencyChange={(currency) => setFrom(currency)}
                             selectCurrency={from}
                             onAmountChange={(amount) => setAmount(amount)}
                         />
@@ -54,7 +55,7 @@ function App() {
                     <div className="relative w-full h-0.5">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-gray-500 text-white px-2 py-0.5"
                             onClick={swap}
                         >
                             swap
@@ -62,15 +63,16 @@ function App() {
                     </div>
                     <div className="w-full mt-1 mb-4">
                         <InputBox
+                            className='font-bold text-xl'
                             label="To"
                             amount={convertedAmount}
                             currencyOptions={options}
                             onCurrencyChange={(currency) => setTo(currency)}
-                            selectCurrency={from}
+                            selectCurrency={to}
                             amountDisable
                         />
                     </div>
-                    <button type="submit" className="w-full bg-gradient-to-r from-blue-800 to-green-600 text-white px-4 py-3 rounded-lg">
+                    <button type="submit" className="w-full bg-slate-500 text-white px-4 py-3 rounded-lg">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
