df6c358aec7bb7f3c1e9646f58fa3ddff9eb17ab
SakshiChak <mailme.sakshichak@gmail.com>
2024-08-25 16:47:41 +0530

    install tailwind css

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,37 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
 
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.finally(() => setLoading(false));
+	}, []);
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+	return !loading ? (
+		<div className="min-h-screen  flex flex-col justify-between bg-gradient-to-r from-gray-400 to-gray-600 text-white">
+			<Header />
+			<main className="flex-grow flex justify-center items-center">
+				<p className="text-xl">Loading...</p>
+			</main>
+			<Footer />
+		</div>
+	) : null;
 }
 
-export default App
+export default App;
